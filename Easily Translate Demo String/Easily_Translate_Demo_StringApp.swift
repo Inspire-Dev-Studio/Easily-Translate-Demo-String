c262e9a3abd384993b8c83953560eaf74ca6ae54
quickcaptions7 <quickcaptions7@gmail.com>
2025-01-09 12:17:52 +0530

    Initial Commit

---
+++ Easily Translate Demo String/Easily_Translate_Demo_StringApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easily_Translate_Demo_StringApp.swift
+//  Easily Translate Demo String
+//
+//  Created by Ajay Mann on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Easily_Translate_Demo_StringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
